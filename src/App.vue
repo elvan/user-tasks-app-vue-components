2d8841c6e6f90f26c616c0e40c56973238705355
Elvan H <elvanhd@gmail.com>
2022-01-27 10:35:52 +0700

    local component registration

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,17 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfoVue from './components/UserInfo.vue';
+
 export default {
+  components: {
+    'the-header': TheHeader,
+    'badge-list': BadgeList,
+    'user-info': UserInfoVue,
+  },
+
   data() {
     return {
       activeUser: {
