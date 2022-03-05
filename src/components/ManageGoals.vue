fa17d7e75d03a864c180d4f4c9d542145ffc4df0
Elvan H <elvanhd@gmail.com>
2022-03-05 23:54:28 +0700

    creating an error alert dialog

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,39 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <error-alert v-if="inputIsInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Please enter at least a few characters</p>
+    <button @click="closeErrorAlert">Close</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: {
+    'error-alert': ErrorAlert,
+  },
+
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+
+    closeErrorAlert() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
