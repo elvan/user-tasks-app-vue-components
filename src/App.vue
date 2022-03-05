f25c05d61efb1044bbf0815b85dd49256ab1fe3d
Elvan H <elvanhd@gmail.com>
2022-03-05 22:05:58 +0700

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,57 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <!-- <badge-list></badge-list> -->
+    <!-- <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
+    ></user-info> -->
+    <!-- <course-goals #default="slotProps">
       <h3>{{ slotProps.goal }}</h3>
       <p>{{ slotProps.anotherProp }}</p>
-    </course-goals>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
+    <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfoVue from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+// import UserInfoVue from './components/UserInfo.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
     'the-header': TheHeader,
-    'badge-list': BadgeList,
-    'user-info': UserInfoVue,
-    'course-goals': CourseGoals,
+    // 'badge-list': BadgeList,
+    // 'course-goals': CourseGoals,
+    // 'user-info': UserInfoVue,
+    'active-goals': ActiveGoals,
+    'manage-goals': ManageGoals,
   },
 
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
+        name: 'Elvan Hidayat',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    },
   },
 };
 </script>
