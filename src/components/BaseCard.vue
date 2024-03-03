7f538b2bbe9156e0a187894460046ac0b71c67c3
Elvan Hidayat <elvanhd@gmail.com>
2024-03-03 11:37:46 +0700

    Introduce BaseCard component for enhanced UI consistency
    - Add BaseCard component to serve as a reusable UI container
      - Implement BaseCard with a slot for flexible content insertion
      - Style BaseCard for consistency across the application
    - Update UserInfo component to utilize BaseCard
      - Wrap UserInfo content within BaseCard to leverage common styling
      - Remove redundant styling from UserInfo, now inherited from BaseCard
    - Register BaseCard globally in main.js for application-wide access
      - Import and register BaseCard alongside existing global components

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  max-width: 30rem;
+  margin: 2rem auto;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,12 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
@@ -15,14 +17,6 @@ export default {
 </script>
 
 <style scoped>
-section {
-  max-width: 30rem;
-  margin: 2rem auto;
-  padding: 1rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-}
-
 section header {
   display: flex;
   justify-content: space-between;
