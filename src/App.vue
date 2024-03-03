c69e23d479df4c7f43521565e6882e47e4edca25
Elvan Hidayat <elvanhd@gmail.com>
2024-03-03 12:23:55 +0700

    Integrate CourseGoals component with scoped slots
    - Import CourseGoals component into App.vue
    - Use CourseGoals component in App.vue with scoped slots
      - Utilize scoped slot syntax to pass item and additional props
    - Create CourseGoals component with a list of goals
      - Implement scoped slots to allow parent component to customize content
      - Pass each goal as a slot prop along with an additional prop for demonstration

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps['another-prop'] }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
 import TheHeader from './components/TheHeader.vue';
 import UserInfo from './components/UserInfo.vue';
 
@@ -20,6 +25,7 @@ export default {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
